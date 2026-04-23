e8c2f2b734c3dd400a44d609422cb97d590ecb49
Eddie Re <edward.d.re@gmail.com>
2026-04-23 16:12:13 -0400

    added model

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Html } from '@react-three/drei'
 import { useState } from 'react'
+import { useGLTF } from '@react-three/drei'
 import './App.css'
+
+function Model() {
+  const { scene } = useGLTF('/models/model.glb')
+
+  return (
+    <primitive 
+      object={scene} 
+      scale={1.2} 
+      position={[0, -1.5, 0]} 
+    />
+  )
+}
 
 function BodyMarker({ position, label, onSelect }) {
   return (
@@ -27,63 +40,6 @@ function BodyMarker({ position, label, onSelect }) {
   )
 }
 
-function SimpleBody({ onSelect }) {
-  return (
-    <group>
-      {/* torso */}
-      <mesh position={[0, 0, 0]}>
-        <capsuleGeometry args={[0.45, 1.6, 8, 16]} />
-        <meshStandardMaterial />
-      </mesh>
-
-      {/* head */}
-      <mesh position={[0, 1.35, 0]}>
-        <sphereGeometry args={[0.28, 32, 32]} />
-        <meshStandardMaterial />
-      </mesh>
-
-      {/* left arm */}
-      <mesh position={[-0.7, 0.45, 0]} rotation={[0, 0, Math.PI / 2]}>
-        <capsuleGeometry args={[0.12, 1.0, 8, 16]} />
-        <meshStandardMaterial />
-      </mesh>
-
-      {/* right arm */}
-      <mesh position={[0.7, 0.45, 0]} rotation={[0, 0, Math.PI / 2]}>
-        <capsuleGeometry args={[0.12, 1.0, 8, 16]} />
-        <meshStandardMaterial />
-      </mesh>
-
-      {/* left leg */}
-      <mesh position={[-0.22, -1.35, 0]}>
-        <capsuleGeometry args={[0.14, 1.3, 8, 16]} />
-        <meshStandardMaterial />
-      </mesh>
-
-      {/* right leg */}
-      <mesh position={[0.22, -1.35, 0]}>
-        <capsuleGeometry args={[0.14, 1.3, 8, 16]} />
-        <meshStandardMaterial />
-      </mesh>
-
-      {/* clickable regions */}
-      <BodyMarker position={[-0.55, 0.65, 0.18]} label="left shoulder" onSelect={onSelect} />
-      <BodyMarker position={[0.55, 0.65, 0.18]} label="right shoulder" onSelect={onSelect} />
-      <BodyMarker position={[-0.42, 0.1, 0.18]} label="left wrist" onSelect={onSelect} />
-      <BodyMarker position={[0.42, 0.1, 0.18]} label="right wrist" onSelect={onSelect} />
-      <BodyMarker position={[-0.22, -0.75, 0.18]} label="left hip" onSelect={onSelect} />
-      <BodyMarker position={[0.22, -0.75, 0.18]} label="right hip" onSelect={onSelect} />
-      <BodyMarker position={[-0.22, -1.35, 0.18]} label="left knee" onSelect={onSelect} />
-      <BodyMarker position={[0.22, -1.35, 0.18]} label="right knee" onSelect={onSelect} />
-      <BodyMarker position={[-0.22, -2.0, 0.18]} label="left ankle/foot" onSelect={onSelect} />
-      <BodyMarker position={[0.22, -2.0, 0.18]} label="right ankle/foot" onSelect={onSelect} />
-      <BodyMarker position={[0, -0.2, -0.22]} label="back" onSelect={onSelect} />
-      <BodyMarker position={[-0.62, 0.28, 0.18]} label="left hand" onSelect={onSelect} />
-      <BodyMarker position={[0.62, 0.28, 0.18]} label="right hand" onSelect={onSelect} />
-    </group>
-  )
-}
-
 export default function App() {
   const [selectedLocation, setSelectedLocation] = useState('none')
 
@@ -93,7 +49,7 @@ export default function App() {
         <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
           <ambientLight intensity={1.4} />
           <directionalLight position={[4, 5, 4]} intensity={1.5} />
-          <SimpleBody onSelect={setSelectedLocation} />
+          <Model onSelect={setSelectedLocation} />
           <OrbitControls enablePan={false} />
         </Canvas>
       </div>
